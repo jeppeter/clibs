cc1c4f433952f3a6091eb2f97c5a1410ab04ed60
jeppeter <jeppeter@gmail.com>
2018-05-09 16:14:17 +0800

    to add ux_fileop

---
+++ uxlib/ux_fileop.h
@@ -0,0 +1,14 @@
+#ifndef __UX_FILEOP_H_36C20057D60A2BC5108FF8E12352DCE4__
+#define __UX_FILEOP_H_36C20057D60A2BC5108FF8E12352DCE4__
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus*/
+
+int read_file_whole(char* infile,char** ppoutbuf,int *bufsize);
+
+#ifdef __cplusplus
+};
+#endif /* __cplusplus*/
+
+#endif /* __UX_FILEOP_H_36C20057D60A2BC5108FF8E12352DCE4__ */
