283e36405f9c2cc3104438cd0406ae7c8ee450cb
jeppeter <jeppeter@gmail.com>
2018-05-10 09:34:56 +0800

    to make the addstring ok

--- test/cmntest/main.cpp
+++ test/cmntest/main.cpp
@@ -2,6 +2,7 @@
 #include <cmn_args.h>
 #include <cmn_fileop.h>
 #include <cmn_output_debug.h>
+#include <cmn_strop.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -48,33 +49,33 @@ int init_log_verbose(pargs_options_t pargs)
     return 0;
 }
 
-int read_input(pargs_options_t pargs, char** ppoutbuf,int *pbufsize)
+int read_input(pargs_options_t pargs, char** ppoutbuf, int *pbufsize)
 {
-	if (pargs == NULL) {
+    if (pargs == NULL) {
         SETERRNO(-1);
-		return -1;
-	}
-	if (pargs->m_input == NULL) {
-		return read_stdin_whole(0,ppoutbuf,pbufsize);
-	}
+        return -1;
+    }
+    if (pargs->m_input == NULL) {
+        return read_stdin_whole(0, ppoutbuf, pbufsize);
+    }
 
-	return read_file_whole(pargs->m_input,ppoutbuf,pbufsize);
+    return read_file_whole(pargs->m_input, ppoutbuf, pbufsize);
 }
 
-int write_output(pargs_options_t pargs, char* poutbuf,int outsize)
+int write_output(pargs_options_t pargs, char* poutbuf, int outsize)
 {
     int ret;
-    if (pargs==NULL) {
+    if (pargs == NULL) {
         ret = -CMN_EINVAL;
         SETERRNO(ret);
         return ret;
     }
 
-    if (pargs->m_output !=NULL) {
-        return write_file_whole(pargs->m_output, poutbuf,outsize);
+    if (pargs->m_output != NULL) {
+        return write_file_whole(pargs->m_output, poutbuf, outsize);
     }
 
-    return write_out_whole(STDOUT_FILE_FLAG, poutbuf,outsize);
+    return write_out_whole(STDOUT_FILE_FLAG, poutbuf, outsize);
 }
 
 void free_input(pargs_options_t pargs, char** ppoutbuf, int *pbufsize)
@@ -83,101 +84,140 @@ void free_input(pargs_options_t pargs, char** ppoutbuf, int *pbufsize)
         return;
     }
     if (pargs->m_input == NULL) {
-        read_stdin_whole(1,ppoutbuf,pbufsize);
+        read_stdin_whole(1, ppoutbuf, pbufsize);
         return;
     }
-    read_file_whole(NULL,ppoutbuf,pbufsize);
+    read_file_whole(NULL, ppoutbuf, pbufsize);
     return;
+}
+
+int pretty_write_output(jvalue* pj, pargs_options_t pargs)
+{
+    char* poutstr = NULL;
+    char* poutbuf =NULL ;
+    int outsize = 0;
+    int outlen=0;
+    int strsize = 0;
+    int ret;
+
+    poutstr = jvalue_write_pretty(pj, (unsigned int*)&strsize);
+    if (poutstr == NULL) {
+        GETERRNO(ret);
+        fprintf(stderr, "can not pretty out error[%d]\n", ret);
+        goto fail;
+    }
+
+    ret = snprintf_safe(&poutbuf,&outsize,"%s\n", poutstr);
+    if (ret < 0) {
+        GETERRNO(ret);
+        fprintf(stderr,"can not add line error[%d]\n",ret);
+        goto fail;
+    }
+    outlen = ret;
+
+    ret = write_output(pargs, poutbuf, outlen);
+    if (ret < 0) {
+        GETERRNO(ret);
+        fprintf(stderr, "can not write output error[%d]\n", ret);
+        goto fail;
+    }
+
+    snprintf_safe(&poutbuf,&outsize,NULL);
+    if(poutstr != NULL) {
+        free(poutstr);
+    }
+    poutstr = NULL;
+    strsize = 0;
+    outlen = 0;
+    return ret;
+
+fail:
+    snprintf_safe(&poutbuf,&outsize,NULL);
+    if(poutstr != NULL) {
+        free(poutstr);
+    }
+    poutstr = NULL;
+    strsize = 0;
+    outlen = 0;
+    SETERRNO(ret);
+    return ret;
 }
 
 
 int addstring_handler(int argc, char* argv[], pextargs_state_t parsestate, void* popt)
 {
-	args_options_t * pargs= (args_options_t*) popt;
-	int ret;
-	char* preadbuf=NULL;
-	int readsize=0;
-    int retlen=0;
-    int argcnt=0;
-    jvalue* pj=NULL;
-    char* pkey=NULL;
-    char* pstr=NULL;
-    char* poutstr=NULL;
-    int outsize=0;
+    args_options_t * pargs = (args_options_t*) popt;
+    int ret;
+    char* preadbuf = NULL;
+    int readsize = 0;
+    int retlen = 0;
+    int argcnt = 0;
+    jvalue* pj = NULL;
+    char* pkey = NULL;
+    char* pstr = NULL;
+    int i;
 
     argc = argc;
     argv = argv;
 
-	ret = init_log_verbose(pargs);
-	if (ret < 0){
-		goto out;
-	}
+    ret = init_log_verbose(pargs);
+    if (ret < 0) {
+        goto out;
+    }
 
 
-    if (parsestate->leftargs == NULL) {
-        while(parsestate->leftargs[argcnt] != NULL) {
+    if (parsestate->leftargs != NULL) {
+        while (parsestate->leftargs[argcnt] != NULL) {
             argcnt ++;
         }
     }
 
-    if (argcnt < 2) {
+    if (argcnt < 2 || (argcnt % 2) != 0) {
         ret = -CMN_EINVAL;
-        fprintf(stderr,"must at least 2 args for addstring\n");
+        fprintf(stderr, "must at least 2 args for addstring[%d]\n", argcnt);
         goto out;
     }
-    pkey = parsestate->leftargs[0];
-    pstr = parsestate->leftargs[1];
+    ret = read_input(pargs, &preadbuf, &readsize);
+    if (ret < 0) {
+        GETERRNO(ret);
+        fprintf(stderr, "can not read [%s] error[%d]\n", pargs->m_input ? pargs->m_input : "stdin", ret);
+        goto out;
+    }
 
-	ret = read_input(pargs,&preadbuf,&readsize);
-	if (ret < 0) {
-		GETERRNO(ret);
-		fprintf(stderr,"can not read [%s] error[%d]\n", pargs->m_input ? pargs->m_input : "stdin", ret);
-		goto out;
-	}
+    fprintf(stdout, "read json\n-----------------\n%s\n+++++++++++++++++\n", preadbuf);
 
-    fprintf(stdout,"read json\n-----------------\n%s+++++++++++++++++\n", preadbuf);
-
-    pj = jvalue_read(preadbuf,(unsigned int*)&retlen);
+    pj = jvalue_read(preadbuf, (unsigned int*)&retlen);
     if (pj == NULL) {
         GETERRNO(ret);
-        fprintf(stderr,"parse error[%d]\n",ret);
+        fprintf(stderr, "parse error[%d]\n", ret);
         goto out;
     }
 
-    ret = jobject_put_string(pj,pkey,pstr);
+    for (i = 0; i < argcnt ; i += 2) {
+        pkey = parsestate->leftargs[i];
+        pstr = parsestate->leftargs[i+1];
+
+        ret = jobject_put_string(pj, pkey, pstr);
+        if (ret < 0) {
+            GETERRNO(ret);
+            fprintf(stderr, "can not insert key[%s]=[%s] error[%d]\n", pkey, pstr, ret);
+            goto out;
+        }
+    }
+
+    ret = pretty_write_output(pj,pargs);
     if (ret < 0) {
         GETERRNO(ret);
-        fprintf(stderr,"can not insert key[%s]=[%s] error[%d]\n", pkey,pstr,ret);
-        goto out;
-    }
-
-    poutstr = jvalue_write_pretty(pj,(unsigned int*)&outsize);
-    if (poutstr == NULL) {
-        GETERRNO(ret);
-        fprintf(stderr,"can not pretty out error[%d]\n",ret);
-        goto out;
-    }
-
-    ret = write_output(pargs, poutstr, outsize);
-    if (ret < 0) {
-        GETERRNO(ret);
-        fprintf(stderr,"can not write output error[%d]\n",ret);
         goto out;
     }
     ret = 0;
-
 out:
-    if (poutstr != NULL) {
-        free(poutstr);
-    }
-    poutstr = NULL;
-    if (pj !=NULL) {
+    if (pj != NULL) {
         jvalue_destroy(pj);
     }
     pj = NULL;
-
-	free_input(pargs,&preadbuf,&readsize);
-	SETERRNO(ret);
+    free_input(pargs, &preadbuf, &readsize);
+    SETERRNO(ret);
     return ret;
 }
 
