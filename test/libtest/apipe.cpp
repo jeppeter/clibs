d619ee6bdeb01a15775a9f551c44685f524f0528
jeppeter <jeppeter@gmail.com>
2018-05-21 13:46:55 +0800

    to add apipe

--- test/libtest/apipe.cpp
+++ test/libtest/apipe.cpp
@@ -7,7 +7,14 @@ void __free_async_evt(pasync_evt_t* ppevt)
 	int res;
 	if (ppevt != NULL && *ppevt != NULL) {
 		pasync_evt_t pevt = *ppevt;
-		CHECK_CLOSE_HANDLE_FMT(pevt->m_evt,"close %p error[%d]", pevt->m_evt, res);
+		if (pevt->m_evt != NULL) {
+			bret = CloseHandle(pevt->m_evt);
+			if (!bret) {
+				GETERRNO(res);
+				ERROR_INFO("close handle [%p] error[%d]", pevt->m_evt, res);
+			}
+			pevt->m_evt = NULL;
+		}
 		pevt->m_cbret = 0;
 		pevt->m_errorcode = 0;
 		free(pevt);
@@ -20,12 +27,22 @@ pasync_evt_t __alloc_async_evt()
 {
 	pasync_evt_t pevt=NULL;
 	int ret;
+
+	pevt = (pasync_evt_t)malloc(sizeof(*pevt));
+	if (pevt == NULL) {
+		GETERRNO(ret);
+		ERROR_INFO("alloc %d error[%d]", sizeof(*pevt), ret);
+		goto fail;
+	}
 	
-	ALLOC_PTR_TYPEOF(pevt);
 	memset(pevt,0, sizeof(*pevt));
 	pevt->m_evt = CreateEvent(NULL,TRUE,TRUE,NULL);
-	CHECK_NULL_FAIL(pevt->m_evt,"can not create event error[%d]",ret);
-	
+	if (pevt->m_evt == NULL) {
+		GETERRNO(ret);
+		ERROR_INFO("can not create event error[%d]",ret);
+		goto fail;
+	}
+
 	return pevt;
 fail:
 	__free_async_evt(&pevt);
@@ -33,11 +50,12 @@ fail:
 }
 
 
-int __create_pipe_async(char* name , int wr, HANDLE *ppipe)
+int __create_pipe_async(char* name , int wr, HANDLE *ppipe,OVERLAPPED* pov, int bufsize)
 {
 	TCHAR* ptname=NULL;
 	int tnamesize=0;
 	BOOL bret;
+	int res;
 	if (name == NULL || ppipe == NULL || *ppipe != NULL || pov == NULL || pstate == NULL) {
 		ret = -ERROR_INVALID_PARAMETER;
 		SETERRNO(ret);
@@ -51,11 +69,32 @@ int __create_pipe_async(char* name , int wr, HANDLE *ppipe)
 	}
 
 	/*now we should make the */
+	ret = AnsiToTchar(name,&ptname,&tnamesize);
+	if (ret < 0) {
+		GETERRNO(ret);
+		goto fail;
+	}
+
+	*ppipe = CreateNamedPipe(ptname,PIPE_ACCESS_DUPLEX| FILE_FLAG_OVERLAPPED, PIPE_WAIT,1,bufsize,bufsize,PIPE_TIMEOUT,NULL);
+	if (*ppipe ==NULL) {
+		GETERRNO(ret);
+		ERROR_INFO("can not create [%s] error[%d]", name,ret);
+		goto fail;
+	}
+
+	bret = ConnectNamedPipe(*ppipe,pov);
+	if (!bret) {
+		GETERRNO(ret);
+		if (ret != -ERROR_IO_PENDING && ret != -ERROR_PIPE_CONNECTED) {
+			ERROR_INFO("can not connect [%s] error[%d]", name,ret);
+			goto fail;
+		}
+	}
 
 
 	return 0;
 fail:
-
+	CHECK_CLOSE_HANDLE_BARE(*ppipe);
 	SETERRNO(ret);
 	return ret;       
 }
--- winlib/win_err.h
+++ winlib/win_err.h
@@ -36,51 +36,5 @@
 	}                                                         \
 	while(0)
 
-#define ALLOC_ERROR_GOTO(type,ptr,size,label)                 \
-    do{                                                       \
-    	ptr = (type*) malloc((size_t) (size));                \
-    	if ((ptr) == NULL) {                                  \
-    		GETERRNO(ret);                                    \
-    		ERROR_INFO("alloc %d error[%d]", (size), ret);    \
-    		goto label;                                       \
-    	}                                                     \
-    }while(0)
-
-#define ALLOC_SIZEOF_GOTO(type, ptr, label)  ALLOC_ERROR_GOTO(type,ptr, sizeof(type),label)
-#define ALLOC_SIZEOF(type,ptr)               ALLOC_SIZEOF_GOTO(type,ptr,fail)
-#define ALLOC_ERROR(type,ptr,size)           ALLOC_ERROR_GOTO(type,ptr,size,fail)
-#define ALLOC_PTR_TYPEOF(ptr)                ALLOC_SIZEOF(typeof(ptr),ptr)                     
-
-
-#define FREE_CHECK(ptr)                                       \
-    do{                                                       \
-    	if ((ptr) != NULL) {                                  \
-    		free((ptr));                                      \
-    	}                                                     \
-    	ptr = NULL;                                           \
-    }while(0)
-
-#define CHECK_NULL_LABEL_FMT(ptr, label,...)                  \
-    do{                                                       \
-    	if ((ptr) == NULL) {                                  \
-    		GETERRNO(ret);                                    \
-    		ERROR_INFO(__VA_ARGS__);                          \
-    		goto label;                                       \
-    	}                                                     \
-    }while(0)
-
-#define CHECK_NULL_FAIL(ptr,...)             CHECK_NULL_LABEL_FMT(ptr,fail,__VA_ARGS__)
-#define CHECK_CLOSE_HANDLE_FMT(hdl,...)                       \
-    do{                                                       \
-    	BOOL _bret;                                           \
-    	if (hdl != NULL && hdl != INVALID_HANDLE_VALUE) {     \
-    		_bret = CloseHandle((hdl));                       \
-    		if (!_bret) {                                     \
-    			GETERRNO(res);                                \
-    			ERROR_INFO(__VA_ARGS__);                      \
-    		}                                                 \
-    	}                                                     \
-    }while(0)
-
 
 #endif /*__WIN_ERR_H__*/
