2a3c7299da777e918d44cfcf90cc55b78d5716fe
jeppeter <jeppeter@gmail.com>
2019-07-24 14:03:58 +0800

    to make log_rcv.cpp coding ,will check code

--- test/loglib/log_rcv.cpp
+++ test/loglib/log_rcv.cpp
@@ -1,14 +1,29 @@
 #include <log_rcv.h>
 
+
+typedef struct __dbwin_buffer {
+	DWORD procid;
+	char data[4096- sizeof(DWORD)];
+} dbwin_buffer_t,*pdbwin_buffer_t;
+
+
 LogMonitor::LogMonitor(void* pevmain,int global)
 {
 	this->m_pevmain = pevmain;
 	this->m_global = global;
 	this->m_pcallback = NULL;
+	this->m_buffready = NULL;
+	this->m_dataready = NULL;
+	this->m_mapbuf = NULL;
+	this->m_inserted = 0;
+	this->m_pcurdata = NULL;
+	this->m_cursize = 0;
+	this->m_curlen = 0;
 }
 
 void LogMonitor::__remove_all_callbacks()
 {
+
 	if (this->m_pcallback) {
 		while(this->m_pcallback->size() > 0) {
 			this->m_pcallback->erase(this->m_pcallback->begin());
@@ -19,10 +34,85 @@ void LogMonitor::__remove_all_callbacks()
 	return;
 }
 
+void LogMonitor::__unmap_buf(void)
+{
+	unmap_buffer(&(this->m_mapbuf));
+}
+
+void LogMonitor::__remove_data_ready_evt(void)
+{
+	int ret;
+	if (this->m_inserted) {
+		ret = libev_remove_handle(this->m_pevmain,this->m_dataready);
+		ASSERT_IF(ret > 0);
+		this->m_inserted = 0;
+	}
+	return;
+}
+
+int LogMonitor::__insert_data_ready_evt(void)
+{
+	int ret;
+	ASSERT_IF(this->m_inserted == 0);
+	ret = libev_insert_handle(this->m_pevmain,this->m_dataready,LogMonitor::handle_data_ready,this,INFINIT_TIME);
+	ASSERT_IF(ret > 0);
+	this->m_inserted = 1;
+	return 0;
+}
+
+
+void LogMonitor::__close_event(void)
+{
+	this->__remove_data_ready_evt();
+	if (this->m_dataready != NULL) {
+		CloseHandle(this->m_dataready);
+		this->m_dataready = NULL;
+	}
+
+	if (this->m_buffready != NULL) {
+		CloseHandle(this->m_buffready);
+		this->m_buffready = NULL;
+	}
+	return ;
+}
+
+void LogMonitor::__free_buf(void)
+{
+	if (this->m_pcurdata) {
+		free(this->m_pcurdata);
+	}
+	this->m_pcurdata = NULL;
+	this->m_curlen = 0;
+	this->m_cursize = 0;
+}
+
+int LogMonitor::__alloc_buf(void)
+{
+	this->__free_buf();
+	this->m_cursize = (sizeof(dbwin_buffer_t) - sizeof(DWORD));
+	this->m_pcurdata = malloc(this->m_cursize);
+	if (this->m_pcurdata == NULL) {
+		GETERRNO(ret);
+		goto fail;
+	}
+	this->m_curlen = 0;
+
+	return 0;
+fail:
+	this->__free_buf();
+	SETERRNO(ret);
+	return ret;
+}
 
 LogMonitor::~LogMonitor()
 {
 	this->__remove_all_callbacks();
+	this->__unmap_buf();
+	this->__close_event();
+	this->__free_buf();
+
+	this->m_pevmain = NULL;
+	this->m_global = 0;
 }
 
 int LogMonitor::add_log_callback(LogCallback* pcallback)
@@ -30,6 +120,10 @@ int LogMonitor::add_log_callback(LogCallback* pcallback)
 	int ret;
 	int findidx=-1;
 	DWORD i;
+	if(this->m_pcallback == NULL) {
+		this->m_pcallback = new std::vector<LogCallback*>();
+	}
+
 	if (this->m_pcallback == NULL) {
 		ret = -ERROR_APP_INIT_FAILURE;
 		SETERRNO(ret);
@@ -70,4 +164,193 @@ int LogMonitor::remove_log_callback(LogCallback* pcallback)
 		}
 	}
 	return ret;
+}
+
+HANDLE LogMonitor::__create_event_name(char* name)
+{
+	int ret;
+	char* psname = NULL;
+	int snamesize=0;
+	HANDLE evt=NULL;
+
+	if (this->m_global) {
+		ret = snprintf_safe(&psname,&snamesize,"Global\\%s", name);
+	} else {
+		ret = snprintf_safe(&psname,&snamesize,"%s", name);
+	}
+	if (ret < 0) {
+		GETERRNO(ret);
+		goto fail;
+	}
+
+	evt = get_or_create_event(psname);
+	if (evt == NULL) {
+		GETERRNO(ret);
+		goto fail;
+	}
+
+	snprintf_safe(&psname,&snamesize,NULL);
+	return 
+fail:
+	if (evt != NULL) {
+		CloseHandle(evt);
+	}
+	evt = NULL;
+	snprintf_safe(&psname,&snamesize,NULL);
+	SETERRNO(ret);
+	return NULL;
+}
+
+int LogMonitor::__alloc_event()
+{
+	int ret;
+
+	this->m_dataready = this->__create_event_name("DBWIN_DATA_READY");
+	if (this->m_dataready == NULL) {
+		GETERRNO(ret);
+		goto fail;
+	}
+
+	this->m_buffready = this->__create_event_name("DBWIN_BUFFER_READY");
+	if (this->m_buffready == NULL) {
+		GETERRNO(ret);
+		goto fail;
+	}
+
+	return 0;
+fail:
+	this->__close_event();
+	SETERRNO(ret);
+	return ret;
+}
+
+int LogMonitor::__map_buff()
+{
+	int ret;
+	char* sname=NULL;
+	int snamesize=0;
+
+	if (this->m_global) {
+		ret = snprintf_safe(&sname,&snamesize,"Global\\DBWIN_BUFFER");
+	} else {
+		ret = snprintf_safe(&sname,&snamesize,"DBWIN_BUFFER");
+	}
+	if (ret < 0) {
+		GETERRNO(ret);
+		goto fail;
+	}
+
+	ret = map_buffer(sname, WINLIB_MAP_FILE_READ,sizeof(dbwin_buffer_t),&(this->m_mapbuf));
+	if (ret < 0) {
+		GETERRNO(ret);
+		goto fail;
+	}
+
+	snprintf_safe(&sname,&snamesize,NULL);
+	return 0;
+fail:
+	this->__unmap_buf();
+	snprintf_safe(&sname,&snamesize,NULL);
+	SETERRNO(ret);
+	return ret;
+}
+
+
+int LogMonitor::start(void)
+{
+	int ret;
+
+	/*to remove */
+	this->__free_buf();
+	this->__unmap_buf();
+	this->__close_event();
+
+	ret = this->__alloc_event();
+	if (ret < 0){
+		GETERRNO(ret);
+		goto fail;
+	}
+
+	ret = this->__map_buff();
+	if (ret < 0) {
+		GETERRNO(ret);
+		goto fail;
+	}
+
+	ret = this->__alloc_buf();
+	if (ret < 0) {
+		GETERRNO(ret);
+		goto fail;
+	}
+
+	/*now insert the handle*/
+	ret = this->__insert_data_ready_evt();
+	if (ret < 0) {
+		GETERRNO(ret);
+		goto fail;
+	}
+
+	return 0;
+fail:
+	this->__free_buf();
+	this->__unmap_buf();
+	this->__close_event();
+	SETERRNO(ret);
+	return ret;
+}
+
+
+int LogMonitor::__data_ready_impl(void)
+{
+	int ret;
+	LogCallback* pcallback;
+	ASSERT_IF(this->m_mapbuf != NULL);
+
+	ret = read_buffer(this->m_mapbuf,sizeof(DWORD), this->m_pcurdata,this->m_cursize);
+	if (ret < 0) {
+		GETERRNO(ret);
+		goto fail;
+	}
+
+	if (this->m_pcallback) {
+		DWORD i;
+		this->m_curlen = strlen(this->m_pcurdata);
+		for (i=0;i<this->m_pcallback->size();i++) {
+			pcallback = this->m_pcallback->at(i);
+			ret = pcallback->handle_log_buffer(this->m_pcurdata, this->m_curlen);
+			if (ret < 0) {
+				GETERRNO(ret);
+				goto fail;
+			}
+		}
+	}
+
+	/*now to notify the buff ready*/
+	ASSERT_IF(this->m_buffready != NULL);
+	SetEvent(this->m_buffready);
+
+	return 0;
+fail:
+	SETERRNO(ret);
+	return ret;
+}
+
+void LogMonitor::handle_data_ready(HANDLE hd,libev_enum_event_t evt,void* pevmain, void* args)
+{
+	LogMonitor* pThis = (LogMonitor*) args;
+	if (evt == normal_event) {
+		ret = pThis->__data_ready_impl();
+		if (ret < 0) {
+			ret = pThis->start();
+			if (ret < 0) {
+				libev_break_winev_loop(pevmain);
+			}
+		}
+	} else {
+		ret = pThis->start();
+		if (ret < 0) {
+			libev_break_winev_loop(pevmain);
+		}
+	}
+	return;
 }
--- test/loglib/log_rcv.h
+++ test/loglib/log_rcv.h
@@ -18,11 +18,21 @@ public:
 
 private:
 	void __remove_all_callbacks();
+	void __remove_data_ready_evt();
+	HANDLE __create_event_name(char* name);
+	static void handle_data_ready(HANDLE hd,libev_enum_event_t evt,void* pevmain, void* args);
 
 private:
 	void* m_pevmain;
 	int m_global;
 	std::vector<LogCallback*> m_pcallback;
+	HANDLE m_buffready;
+	HANDLE m_dataready;
+	void* m_mapbuf;
+	int m_inserted;
+	char* m_pcurdata;
+	int m_cursize;
+	int m_curlen;
 };
 
 #endif /* __LOG_RCV_H_5A2EE3B5EB2ED26636E87A3752BA8DBC__ */
