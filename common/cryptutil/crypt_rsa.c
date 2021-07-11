56c6ddb49da458d12d81798f5969b66bf60fdc1c
jeppeter <jeppeter@gmail.com>
2021-07-11 12:47:42 +0800

    to add crypt_rsa compiling ok

---
+++ common/cryptutil/crypt_rsa.c
@@ -0,0 +1,653 @@
+/*
+ *  RSA was designed by Ron Rivest, Adi Shamir and Len Adleman.
+ *
+ *  http://theory.lcs.mit.edu/~rivest/rsapaper.pdf
+ *  http://www.cacr.math.uwaterloo.ca/hac/about/chap8.pdf
+ */
+
+#include <crypt_rsa.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#if 0
+#define RSA_DEBUG(...)
+#define RSA_ERROR(...)   do { if(printfunc != NULL) { printfunc(__VA_ARGS__); printfunc("\n");}} while(0)
+#else
+#define RSA_DEBUG(...)   do { if(printfunc != NULL) { printfunc("[%s:%d] ",__FILE__,__LINE__); printfunc(__VA_ARGS__); printfunc("\n");}} while(0)
+#define RSA_ERROR(...)   do { if(printfunc != NULL) { printfunc("[%s:%d] ",__FILE__,__LINE__); printfunc(__VA_ARGS__); printfunc("\n");}} while(0)
+#endif
+
+/*
+ * Initialize an RSA context
+ */
+void rsa_init( rsa_context *ctx,
+               int padding,
+               int hash_id )
+{
+    hash_id = hash_id;
+    padding = padding;
+    memset( ctx, 0, sizeof( rsa_context ) );
+    mpz_init(ctx->N);
+    mpz_init(ctx->E);
+    mpz_init(ctx->D);
+    return ;
+}
+
+int rsa_init_nums(rsa_context* ctx,int bitsize,char* nstr,char* estr,
+                  char* dstr,int radix)
+{
+    ctx->len = bitsize;
+    ctx->ver = 0;
+
+    if (nstr)
+    {
+        mpz_set_str(ctx->N,nstr,radix);
+    }
+    if (estr)
+    {
+        mpz_set_str(ctx->E,estr,radix);
+    }
+
+    if (dstr)
+    {
+        mpz_set_str(ctx->D,dstr,radix);
+    }
+    return 0;
+}
+
+int rsa_init_func(rsa_context * ctx,randfunc_t randfunc,void* arg)
+{
+    ctx->m_rand = randfunc;
+    ctx->m_randarg = arg;
+    return 0;
+}
+
+
+/*
+ * Free the components of an RSA key
+ */
+void rsa_free( rsa_context *ctx )
+{
+    mpz_clear(ctx->N);
+    mpz_clear(ctx->E);
+    mpz_clear(ctx->D);
+    return ;
+}
+
+
+int get_hex_val(unsigned char ch)
+{
+    int val=0;
+    if (ch >= '0' && ch<='9')
+    {
+        val = ch - '0';
+    }
+    else if (ch >= 'a' && ch<='f')
+    {
+        val = ch - 'a' + 10;
+    }
+    else if (ch >= 'A' && ch<='F')
+    {
+        val = ch - 'A' + 10;
+    }
+    return val;
+}
+
+int hex_str_buffer(const char* hexstr,uint8_t *pbuf,uint32_t buflen)
+{
+    uint32_t i,j;
+    uint32_t len=(uint32_t) strlen(hexstr);
+
+    if (len > (buflen*2))
+    {
+        return -1;
+    }
+
+    i = 0;
+    if (len < (buflen *2 ))
+    {
+        while(i<((buflen*2-len)/2))
+        {
+            pbuf[i] = 0;
+            i ++;
+        }
+    }
+
+    j = 0;
+    if (len % 2)
+    {
+        pbuf[i] = (uint8_t)get_hex_val(hexstr[j]);
+        j ++;
+        i ++;
+    }
+
+    while ( i < buflen)
+    {
+        pbuf[i] = (uint8_t)((get_hex_val(hexstr[j])<<4)|(get_hex_val(hexstr[j+1])));
+        j += 2;
+        i ++;
+    }
+
+    return buflen;
+}
+
+
+void block_encrypt(mpz_t C, mpz_t M, rsa_context* rsa)
+{
+    /* C = M^e mod n */
+    mpz_powm(C, M, rsa->E, rsa->N);
+    return;
+}
+
+
+int __rsa_encrypt(char cipher[],int cipherlen, char message[], int length, rsa_context* rsa,int blocksize,printf_func_t printfunc)
+{
+    char *expbuf=NULL;
+    char *filledbuf=NULL;
+    int leftlen=length;
+    int leftcipherlen=cipherlen;
+    int filledlen=0;
+    int curlen;
+    char* pptr;
+    uint8_t *ppcipherptr;
+    int i;
+    mpz_t m;
+    mpz_t c;
+    int ret;
+
+    RSA_DEBUG(" ");
+    expbuf = malloc(blocksize*4);
+    if (expbuf == NULL)
+    {
+        goto fail;
+    }
+
+    filledbuf = malloc(blocksize* 2);
+    if (filledbuf == NULL)
+    {
+        goto fail;
+    }
+    RSA_DEBUG(" ");
+
+    leftlen = length;
+    pptr = message;
+    ppcipherptr =(uint8_t*) cipher;
+    filledlen = 0;
+
+    while(leftlen > 0)
+    {
+        curlen  = (blocksize - 3);
+        if (leftlen < curlen)
+        {
+            curlen = leftlen;
+        }
+
+        filledbuf[0] = 0x0;
+        filledbuf[1] = 0x2;
+
+        for (i=2; i<(blocksize - curlen-1); i++)
+        {
+            if (rsa->m_rand)
+            {
+fill_again:
+                filledbuf[i] =(uint8_t) rsa->m_rand(rsa->m_randarg);
+                if (filledbuf[i] == 0)
+                {
+                    goto fill_again;
+                }
+            }
+            else
+            {
+                filledbuf[i] = 0x11;
+            }
+        }
+
+        filledbuf[i] = 0x0;
+        i ++;
+
+        memcpy(&(filledbuf[i]),pptr,curlen);
+
+        RSA_DEBUG(" ");
+        /*now to filled m c */
+        mpz_init(m);
+        RSA_DEBUG(" ");
+        mpz_init(c);
+        RSA_DEBUG(" ");
+
+        mpz_import(m,blocksize,1,sizeof(char),0,0,filledbuf);
+        RSA_DEBUG(" ");
+        block_encrypt(c,m,rsa);
+        RSA_DEBUG(" ");
+
+        if (leftcipherlen < blocksize)
+        {
+            RSA_ERROR(" ");
+            goto fail;
+        }
+
+        RSA_DEBUG(" ");
+        ret = hex_str_buffer(mpz_get_str(expbuf,16,c),ppcipherptr,blocksize);
+        if (ret < 0)
+        {
+            RSA_ERROR(" ");
+            goto fail;
+        }
+
+        RSA_DEBUG(" ");
+        mpz_clear(m);
+        RSA_DEBUG(" ");
+        mpz_clear(c);
+        RSA_DEBUG(" ");
+        leftcipherlen -= blocksize;
+        ppcipherptr += blocksize;
+        pptr += curlen;
+        leftlen -= curlen;
+        filledlen += blocksize;
+    }
+
+    RSA_DEBUG(" ");
+    if (filledbuf)
+    {
+        free(filledbuf);
+    }
+    filledbuf = NULL;
+    if (expbuf)
+    {
+        free(expbuf);
+    }
+    expbuf = NULL;
+    RSA_DEBUG(" ");
+    return filledlen;
+
+fail:
+    if (filledbuf)
+    {
+        free(filledbuf);
+    }
+    filledbuf = NULL;
+    if (expbuf)
+    {
+        free(expbuf);
+    }
+    expbuf = NULL;
+    return -1;
+}
+
+
+int rsa_encrypt(unsigned char cipher[],int cipherlen,unsigned char message[], int messlen, rsa_context* prsa,printf_func_t printfunc)
+{
+    return __rsa_encrypt((char*)cipher,cipherlen,(char*)message,messlen,prsa,(prsa->len >> 3), printfunc);
+}
+
+void block_decrypt(mpz_t M, mpz_t C, rsa_context* prsa,printf_func_t printfunc)
+{
+    if (printfunc) {
+        printfunc = printfunc;
+    }
+    mpz_powm(M, C, prsa->D, prsa->N);
+    return;
+}
+
+
+int __rsa_decrypt(char* message,int messlen, char* cipher, int length,rsa_context* prsa,int blocksize,printf_func_t printfunc)
+{
+    char *expbuf=NULL;
+    uint8_t *filledbuf=NULL;
+    int curlen;
+    char* pcurmess=NULL;
+    int leftmesslen=0;
+    int filledlen = 0;
+    int i,j;
+    mpz_t m;
+    mpz_t c;
+    int ret;
+    if (length & (blocksize - 1))
+    {
+        RSA_ERROR("length %d blocksize (%d)", length, blocksize);
+        goto fail;
+    }
+
+    RSA_DEBUG(" ");
+    expbuf = malloc(blocksize*4);
+    if (expbuf == NULL)
+    {
+        goto fail;
+    }
+
+    RSA_DEBUG(" ");
+    filledbuf = malloc(blocksize* 2);
+    if (filledbuf == NULL)
+    {
+        goto fail;
+    }
+
+    RSA_DEBUG(" ");
+    pcurmess = message;
+    leftmesslen = messlen;
+    filledlen = 0;
+
+    for (i=0; i<(length /blocksize); i++)
+    {
+        RSA_DEBUG(" ");
+        mpz_init(m);
+        RSA_DEBUG(" ");
+        mpz_init(c);
+        RSA_DEBUG(" ");
+        mpz_import(c,blocksize,1,sizeof(char),0,0,cipher+i*blocksize);
+        RSA_DEBUG(" ");
+
+        block_decrypt(m,c,prsa, printfunc);
+        RSA_DEBUG(" ");
+        ret = hex_str_buffer(mpz_get_str(expbuf,16,m),(uint8_t*)filledbuf,blocksize);
+        if (ret < 0)
+        {
+            goto fail;
+        }
+        RSA_DEBUG(" ");
+        mpz_clear(m);
+        RSA_DEBUG(" ");
+        mpz_clear(c);
+        RSA_DEBUG(" ");
+
+        for (j=2; j<blocksize; j++)
+        {
+            if (filledbuf[j] == 0x0)
+            {
+                break;
+            }
+        }
+        RSA_DEBUG(" ");
+
+        if (j >= (blocksize-1))
+        {
+            /*this is invalid ,so failed to decrypt*/
+            goto fail;
+        }
+        RSA_DEBUG(" ");
+        j ++;
+        /*current len to copy*/
+        curlen = blocksize - j;
+        if (leftmesslen < curlen)
+        {
+            goto fail;
+        }
+        RSA_DEBUG(" ");
+
+        memcpy(pcurmess,&(filledbuf[j]),curlen);
+        pcurmess += curlen;
+        leftmesslen -= curlen;
+        filledlen += curlen;
+    }
+
+    if (filledbuf)
+    {
+        free(filledbuf);
+    }
+    filledbuf = NULL;
+    RSA_DEBUG(" ");
+    if (expbuf)
+    {
+        free(expbuf);
+    }
+    expbuf = NULL;
+    RSA_DEBUG(" ");
+    return filledlen;
+
+fail:
+    if (filledbuf)
+    {
+        free(filledbuf);
+    }
+    filledbuf = NULL;
+    if (expbuf)
+    {
+        free(expbuf);
+    }
+    expbuf = NULL;
+    return -1;
+}
+
+
+int rsa_decrypt(unsigned char* message,int messlen,unsigned char* cipher, int cipherlen, rsa_context* prsa,printf_func_t printfunc)
+{
+    RSA_DEBUG(" ");
+    return __rsa_decrypt((char*)message,messlen,(char*)cipher,cipherlen,prsa,(prsa->len >> 3), printfunc);
+}
+
+int __rsa_sign(unsigned char* signedmess ,int signedlen,unsigned char *mess,int messlen,rsa_context* prsa,int blocksize,printf_func_t printfunc)
+{
+    int ret;
+    int leftmesslen=messlen,leftsignedmess=signedlen;
+    unsigned char* pcursignedmess=NULL;
+    char* pcurmess=NULL;
+    int curlen;
+    int filledlen=0;
+    char* expbuf=NULL;
+    char* filledbuf=NULL;
+    mpz_t m;
+    mpz_t c;
+    int i;
+
+    expbuf = malloc(blocksize* 4);
+    if (expbuf == NULL)
+    {
+        goto fail;
+    }
+
+    filledbuf = malloc(blocksize*4);
+    if (filledbuf == NULL)
+    {
+        goto fail;
+    }
+
+
+    pcursignedmess = signedmess;
+    pcurmess = (char*)mess;
+    leftmesslen = messlen;
+    filledlen = 0;
+    leftsignedmess = signedlen;
+    while(leftmesslen > 0)
+    {
+        curlen = blocksize - 3;
+        if (curlen > leftmesslen)
+        {
+            curlen = leftmesslen;
+        }
+
+        filledbuf[0] = 0x0;
+        filledbuf[1] = 0x3;
+        for (i=2; i<(blocksize - curlen - 1); i++)
+        {
+            if (prsa->m_rand)
+            {
+fill_again:
+                filledbuf[i] = (uint8_t) prsa->m_rand(prsa->m_randarg);
+                if (filledbuf[i] == 0)
+                {
+                    goto fill_again;
+                }
+            }
+            else
+            {
+                filledbuf[i] = 0x12;
+            }
+        }
+
+        filledbuf[i] = 0x0;
+        i ++;
+        memcpy(&(filledbuf[i]),pcurmess,curlen);
+
+        mpz_init(m);
+        mpz_init(c);
+
+        /*now filled buffer*/
+        mpz_import(m,blocksize,1,sizeof(char),0,0,filledbuf);
+        block_decrypt(c,m,prsa, printfunc);
+        if (leftsignedmess < blocksize)
+        {
+            goto fail;
+        }
+
+        ret = hex_str_buffer(mpz_get_str(expbuf,16,c),pcursignedmess,blocksize);
+        if (ret < 0)
+        {
+            goto fail;
+        }
+
+
+        mpz_clear(m);
+        mpz_clear(c);
+
+        pcurmess += curlen;
+        pcursignedmess += blocksize;
+        leftmesslen -= curlen;
+        leftsignedmess -= blocksize;
+        filledlen += blocksize;
+    }
+
+    if (filledbuf)
+    {
+        free(filledbuf);
+    }
+    filledbuf = NULL;
+    if (expbuf)
+    {
+        free(expbuf);
+    }
+    expbuf = NULL;
+    return filledlen;
+fail:
+    if (filledbuf)
+    {
+        free(filledbuf);
+    }
+    filledbuf = NULL;
+    if (expbuf)
+    {
+        free(expbuf);
+    }
+    expbuf = NULL;
+    return -1;
+}
+
+int rsa_sign(unsigned char* signedmess ,int signedlen,unsigned char *mess,int messlen,rsa_context* prsa,printf_func_t printfunc)
+{
+    return __rsa_sign(signedmess,signedlen,mess,messlen,prsa,(prsa->len >> 3),printfunc);
+}
+
+
+int __rsa_verify(unsigned char * verimess,int verilen,unsigned char * mess,int messlen,rsa_context * prsa,int blocksize
+        ,printf_func_t printfunc)
+{
+    int ret;
+    int leftmesslen=messlen,leftverimesslen=verilen;
+    unsigned char* pcurverimess=NULL;
+    char* pcurmess=NULL;
+    int curlen;
+    int filledlen=0;
+    char* expbuf=NULL;
+    char* filledbuf=NULL;
+    mpz_t m;
+    mpz_t c;
+    int j;
+    RSA_DEBUG(" ");
+
+    expbuf = malloc(blocksize* 4);
+    if (expbuf == NULL)
+    {
+        goto fail;
+    }
+
+    filledbuf = malloc(blocksize*4);
+    if (filledbuf == NULL)
+    {
+        goto fail;
+    }
+
+    if (messlen % (blocksize))
+    {
+        goto fail;
+    }
+
+
+    pcurverimess = verimess;
+    pcurmess = (char*)mess;
+    leftmesslen = messlen;
+    filledlen = 0;
+    leftverimesslen = verilen;
+
+    while(leftmesslen > 0)
+    {
+        mpz_init(m);
+        mpz_init(c);
+        mpz_import(c,blocksize,1,sizeof(char),0,0,pcurmess);
+
+        block_encrypt(m,c,prsa);
+        ret = hex_str_buffer(mpz_get_str(expbuf,16,m),(uint8_t*)filledbuf,blocksize);
+        if (ret < 0)
+        {
+            goto fail;
+        }
+        mpz_clear(m);
+        mpz_clear(c);
+
+        for (j=2; j<blocksize; j++)
+        {
+            if (filledbuf[j] == 0x0)
+            {
+                break;
+            }
+        }
+
+        if (j >= (blocksize-1))
+        {
+            /*this is invalid ,so failed to decrypt*/
+            goto fail;
+        }
+        j ++;
+        /*current len to copy*/
+        curlen = blocksize - j;
+        if (leftverimesslen < curlen)
+        {
+            goto fail;
+        }
+
+        memcpy(pcurverimess,&(filledbuf[j]),curlen);
+        pcurmess += blocksize;
+        leftmesslen -= blocksize;
+        filledlen += curlen;
+        pcurverimess += curlen;
+        leftverimesslen -= curlen;
+    }
+
+    if (filledbuf)
+    {
+        free(filledbuf);
+    }
+    filledbuf = NULL;
+    if (expbuf)
+    {
+        free(expbuf);
+    }
+    expbuf = NULL;
+    return filledlen;
+fail:
+    if (filledbuf)
+    {
+        free(filledbuf);
+    }
+    filledbuf = NULL;
+    if (expbuf)
+    {
+        free(expbuf);
+    }
+    expbuf = NULL;
+    return -1;
+}
+
+int rsa_verify(unsigned char * verimess,int verilen,unsigned char * mess,int messlen,rsa_context * prsa,printf_func_t printfunc)
+{
+    return __rsa_verify(verimess,verilen,mess,messlen,prsa,(prsa->len >> 3),printfunc);
+}
+
+
---
+++ common/cryptutil/crypt_rsa.h
@@ -0,0 +1,177 @@
+#ifndef __CRYPT_RSA_H__
+#define __CRYPT_RSA_H__
+
+#include <crypt_mpn.h>
+
+/*
+ * RSA Error codes
+ */
+#define ERR_RSA_BAD_INPUT_DATA                    -0x0400
+#define ERR_RSA_INVALID_PADDING                   -0x0410
+#define ERR_RSA_KEY_GEN_FAILED                    -0x0420
+#define ERR_RSA_KEY_CHECK_FAILED                  -0x0430
+#define ERR_RSA_PUBLIC_FAILED                     -0x0440
+#define ERR_RSA_PRIVATE_FAILED                    -0x0450
+#define ERR_RSA_VERIFY_FAILED                     -0x0460
+#define ERR_RSA_OUTPUT_TOO_LARGE                  -0x0470
+#define ERR_RSA_RNG_FAILED                        -0x0480
+
+/*
+ * PKCS#1 constants
+ */
+#define SIG_RSA_RAW     0
+#define SIG_RSA_MD2     2
+#define SIG_RSA_MD4     3
+#define SIG_RSA_MD5     4
+#define SIG_RSA_SHA1	5
+#define SIG_RSA_SHA224	14
+#define SIG_RSA_SHA256	11
+#define	SIG_RSA_SHA384	12
+#define SIG_RSA_SHA512	13
+
+#define RSA_PUBLIC      0
+#define RSA_PRIVATE     1
+
+#define RSA_PKCS_V15    0
+#define RSA_PKCS_V21    1
+
+#define RSA_SIGN        1
+#define RSA_CRYPT       2
+
+#define ASN1_STR_CONSTRUCTED_SEQUENCE	"\x30"
+#define ASN1_STR_NULL			        "\x05"
+#define ASN1_STR_OID			        "\x06"
+#define ASN1_STR_OCTET_STRING		    "\x04"
+
+#define OID_DIGEST_ALG_MDX	        "\x2A\x86\x48\x86\xF7\x0D\x02\x00"
+#define OID_HASH_ALG_SHA1	        "\x2b\x0e\x03\x02\x1a"
+#define OID_HASH_ALG_SHA2X	        "\x60\x86\x48\x01\x65\x03\x04\x02\x00"
+
+#define OID_ISO_MEMBER_BODIES	    "\x2a"
+#define OID_ISO_IDENTIFIED_ORG	    "\x2b"
+
+/*
+ * ISO Member bodies OID parts
+ */
+#define OID_COUNTRY_US		        "\x86\x48"
+#define OID_RSA_DATA_SECURITY	    "\x86\xf7\x0d"
+
+/*
+ * ISO Identified organization OID parts
+ */
+#define OID_OIW_SECSIG_SHA1	        "\x0e\x03\x02\x1a"
+
+/*
+ * DigestInfo ::= SEQUENCE {
+ *   digestAlgorithm DigestAlgorithmIdentifier,
+ *   digest Digest }
+ *
+ * DigestAlgorithmIdentifier ::= AlgorithmIdentifier
+ *
+ * Digest ::= OCTET STRING
+ */
+#define ASN1_HASH_MDX					        \
+(							                    \
+    ASN1_STR_CONSTRUCTED_SEQUENCE "\x20"		\
+      ASN1_STR_CONSTRUCTED_SEQUENCE "\x0C"		\
+        ASN1_STR_OID "\x08"				        \
+	  OID_DIGEST_ALG_MDX				        \
+	ASN1_STR_NULL "\x00"				        \
+      ASN1_STR_OCTET_STRING "\x10"			    \
+)
+
+#define ASN1_HASH_SHA1					        \
+    ASN1_STR_CONSTRUCTED_SEQUENCE "\x21"		\
+      ASN1_STR_CONSTRUCTED_SEQUENCE "\x09"		\
+        ASN1_STR_OID "\x05"				        \
+	  OID_HASH_ALG_SHA1				            \
+        ASN1_STR_NULL "\x00"				    \
+      ASN1_STR_OCTET_STRING "\x14"
+
+#define ASN1_HASH_SHA2X					        \
+    ASN1_STR_CONSTRUCTED_SEQUENCE "\x11"		\
+      ASN1_STR_CONSTRUCTED_SEQUENCE "\x0d"		\
+        ASN1_STR_OID "\x09"				        \
+	  OID_HASH_ALG_SHA2X				        \
+        ASN1_STR_NULL "\x00"				    \
+      ASN1_STR_OCTET_STRING "\x00"
+
+typedef unsigned int (*randfunc_t)(void* arg);
+typedef int (*printf_func_t)(const char* fmt, ...);
+
+/**
+ * \brief          RSA context structure
+ */
+typedef struct
+{
+    int ver;                    /*!<  always 0          */
+    int len;                    /*!<  size(N) in chars  */
+
+    mpz_t N;                      /*!<  public modulus    */
+    mpz_t E;                      /*!<  public exponent   */
+    mpz_t D;                      /*!<  private exponent  */
+	randfunc_t m_rand;
+	void* m_randarg;
+}
+rsa_context;
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * \brief          Initialize an RSA context
+ *
+ * \param ctx      RSA context to be initialized
+ * \param padding  RSA_PKCS_V15 or RSA_PKCS_V21
+ * \param hash_id  RSA_PKCS_V21 hash identifier
+ *
+ * \note           The hash_id parameter is actually ignored
+ *                 when using RSA_PKCS_V15 padding.
+ *
+ * \note           Currently, RSA_PKCS_V21 padding
+ *                 is not supported.
+ */
+void rsa_init( rsa_context *ctx,
+               int padding,
+               int hash_id);
+
+
+/**
+ * \brief          Initialize an RSA nums
+ *
+ * \param ctx      RSA context to be initialized
+ * \param bitsize  bitsize to set for rsa
+ * \param nstr    for N in rsa
+ * \param estr    for E in rsa
+ * \param dstr    for D in rsa
+ * \param radix  for radix as the string will convert
+ *
+ */
+int rsa_init_nums(rsa_context* ctx,int bitsize,char* nstr,char* estr,
+                  char* dstr,int radix);
+
+int rsa_init_func(rsa_context* ctx,randfunc_t randfunc,void* arg);
+
+int rsa_encrypt(unsigned char cipher[],int cipherlen,unsigned char message[], int messlen, rsa_context* prsa,printf_func_t printfunc);
+int rsa_decrypt(unsigned char* message,int messlen,unsigned char* cipher, int cipherlen, rsa_context* prsa,printf_func_t printfunc);
+int rsa_sign(unsigned char* signedmess ,int signedlen,unsigned char *mess,int messlen,rsa_context* prsa,printf_func_t printfunc);
+int rsa_verify(unsigned char* verimess ,int verilen,unsigned char *mess,int messlen,rsa_context* prsa,printf_func_t printfunc);
+
+
+
+
+/**
+ * \brief          Free the components of an RSA key
+ *
+ * \param ctx      RSA Context to free
+ */
+void rsa_free( rsa_context *ctx );
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __CRYPT_RSA_H__ */
