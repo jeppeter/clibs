2284daeac25b017426d13ec7ec2aa182c76644b9
jeppeter <jeppeter@gmail.com>
2021-07-27 14:27:15 +0800

    to make spaceshipsvr init

---
+++ test/comtest/spaceshipsvr/spaceshipsvr/dllmain.h
@@ -0,0 +1,10 @@
+﻿// dllmain.h: 模块类的声明。
+
+class CspaceshipsvrModule : public ATL::CAtlDllModuleT< CspaceshipsvrModule >
+{
+public :
+	DECLARE_LIBID(LIBID_spaceshipsvrLib)
+	DECLARE_REGISTRY_APPID_RESOURCEID(IDR_SPACESHIPSVR, "{c1b1ca83-b9bf-4e99-8be6-cc5d16b62570}")
+};
+
+extern class CspaceshipsvrModule _AtlModule;
