362f3f32004dc9bbcc90878621665e0801922a33
jeppeter <jeppeter@gmail.com>
2023-04-10 13:44:08 +0800

    to make debug property

--- winlib/win_usb.cpp
+++ winlib/win_usb.cpp
@@ -1,5 +1,6 @@
 #include <win_usb.h>
 #include <win_err.h>
+#include <win_uniansi.h>
 
 #pragma warning(push)
 #pragma warning(disable:4820)
@@ -20,6 +21,51 @@ DEFINE_GUID(GUID_DEVINTERFACE_USB_DEVICE,
 #pragma comment(lib,"Cfgmgr32.lib")
 #pragma comment(lib,"SetupAPI.lib")
 
+int __get_guid_str(LPGUID pguid, char** ppstr,int *psize)
+{
+	wchar_t* puni=NULL;
+	int ccmax=256;
+	int ret;
+	int retlen = 0;
+
+	if (pguid == NULL) {
+		return UnicodeToAnsi(NULL,ppstr,psize);
+	}
+
+	puni = (wchar_t*) malloc(ccmax * sizeof(wchar_t));
+	if (puni == NULL) {
+		GETERRNO(ret);
+		goto fail;
+	}
+
+	ret = StringFromGUID2((*pguid),puni,ccmax);
+	if (ret == 0) {
+		GETERRNO(ret);
+		goto fail;
+	}
+
+	ret = UnicodeToAnsi(puni,ppstr,psize);
+	if (ret < 0) {
+		GETERRNO(ret);
+		goto fail;
+	}
+
+	retlen = ret;
+	if (puni) {
+		free(puni);
+	}
+	puni = NULL;
+
+	return retlen;
+fail:
+	if (puni) {
+		free(puni);
+	}
+	puni = NULL;
+	SETERRNO(ret);
+	return ret;
+}
+
 int __get_guid_dev(LPGUID pguid,const char* guidstr,pusb_dev_t* ppdata, int *psize)
 {
 	HDEVINFO hinfo = NULL;
@@ -37,9 +83,16 @@ int __get_guid_dev(LPGUID pguid,const char* guidstr,pusb_dev_t* ppdata, int *psi
 	DWORD i;
 	uint8_t* ppropbuf=NULL;
 	ULONG propbufsize=0;
+	ULONG propbuflen=0;
 	CONFIGRET cfgret;
 	DEVPROPTYPE proptype;
 	BOOL bret;
+	char* fmtid = NULL;
+	int fmtmax = 0;
+	char* propansi=NULL;
+	int propansisize=0;
+	wchar_t* pwptr=NULL;
+	ULONG wi = 0;
 	if (pguid == NULL) {
 		if (ppdata && *ppdata) {
 			free(*ppdata);
@@ -58,7 +111,6 @@ int __get_guid_dev(LPGUID pguid,const char* guidstr,pusb_dev_t* ppdata, int *psi
 
 	pretdata = (pusb_dev_t)(*ppdata);
 	retsize = *psize;
-
 
 
 	hinfo = SetupDiGetClassDevs(pguid,NULL,NULL,DIGCF_ALLCLASSES);
@@ -148,19 +200,78 @@ int __get_guid_dev(LPGUID pguid,const char* guidstr,pusb_dev_t* ppdata, int *psi
 			goto fail;
 		}
 		propkeylen = requiresize;
-		for(i=0;i<propkeylen;i++) {
-			propkeylen = propkeysize;
-			cfgret = CM_Get_DevNode_Property(pndata->DevInst,&(propkey[i]),&proptype,ppropbuf,&propkeylen,0);
-			if (cfgret == CR_SUCCESS) {
-				DEBUG_BUFFER_FMT(ppropbuf,propbufsize,"prop [%d]", i);
-			} else {
-				ERROR_INFO("[%ld] return [%ld]", i,cfgret);
+		DEBUG_INFO("propkeylen [%ld]", propkeylen);
+		if (ppropbuf == NULL) {
+			propbufsize = 4;
+			ppropbuf =(uint8_t*) malloc(propbufsize);
+			if (ppropbuf == NULL) {
+				GETERRNO(ret);
+				goto fail;
 			}
 		}
 
+		for(i=0;i<propkeylen;i++) {
+			ret = __get_guid_str(&(propkey[i].fmtid),&fmtid,&fmtmax);
+			if (ret < 0) {
+				GETERRNO(ret);
+				goto fail;
+			}
+		try_again:
+			propbuflen = propbufsize;
+			memset(ppropbuf,0,propbufsize);
+			cfgret = CM_Get_DevNode_PropertyW(pndata->DevInst,&(propkey[i]),&proptype,ppropbuf,&propbuflen,0);
+			if (cfgret == CR_SUCCESS) {
+				pwptr = (wchar_t*)ppropbuf;
+				wi = 0;
+				DEBUG_INFO("[%ld].[%ld] prop [%s] pid [0x%lx:%ld]", nindex,i,fmtid,propkey[i].pid,propkey[i].pid);
+				while ( wi < propbuflen) {
+					while (*pwptr == 0x0 && wi < propbuflen) {
+						pwptr += 1;
+						wi += 2;
+					}
+
+					if (wi < propbuflen) {
+						ret = UnicodeToUtf8(pwptr,&propansi,&propansisize);
+						if (ret < 0) {
+							GETERRNO(ret);
+							goto fail;
+						}
+						DEBUG_INFO("    prop [%s]",propansi);
+					}
+
+					while (*pwptr != 0x0 && wi < propbuflen) {
+						pwptr += 1;
+						wi += 2;
+					}
+				}
+				DEBUG_BUFFER_FMT(ppropbuf,propbuflen,"[%ld].[%d]prop [%s] pid[%ld:0x%lx]", nindex,i, fmtid,propkey[i].pid,propkey[i].pid);
+			} else {
+				if (cfgret == CR_BUFFER_SMALL) {
+					propbufsize <<= 1;
+					if (ppropbuf) {
+						free(ppropbuf);
+					}
+					ppropbuf = (uint8_t*)malloc(propbufsize);
+					if (ppropbuf == NULL) {
+						GETERRNO(ret);
+						goto fail;
+					}
+					goto try_again;
+				} else {
+					ERROR_INFO("[%ld][%ld] return [%ld]",nindex, i,cfgret);	
+				}
+				
+			}
+		}
+
+		nindex ++;
+		retlen ++;
 
 	}
 
+	UnicodeToUtf8(NULL,&propansi,&propansisize);
+
+	__get_guid_str(NULL,&fmtid,&fmtmax);
 
 	if (ppropbuf) {
 		free(ppropbuf);
@@ -184,9 +295,18 @@ int __get_guid_dev(LPGUID pguid,const char* guidstr,pusb_dev_t* ppdata, int *psi
 	}
 	hinfo = NULL;
 
+
+	if (*ppdata && *ppdata != pretdata) {
+		free(*ppdata);
+	}
+	*ppdata = pretdata;
+	*psize = retsize;
 
 	return retlen;
 fail:
+	UnicodeToUtf8(NULL,&propansi,&propansisize);
+
+	__get_guid_str(NULL,&fmtid,&fmtmax);
 
 	if (ppropbuf) {
 		free(ppropbuf);
@@ -208,6 +328,11 @@ fail:
 		SetupDiDestroyDeviceInfoList(hinfo);
 	}
 	hinfo = NULL;
+
+	if (pretdata != *ppdata && pretdata != NULL) {
+		free(pretdata);
+	}
+	pretdata = NULL;
 
 	SETERRNO(ret);
 	return ret;
@@ -217,7 +342,7 @@ int list_usb_roots(int freed, pusb_dev_t* ppur, int *psize)
 {
 	int retlen = 0;
 	char* guidstr = NULL;
-	int ccmax = 256;
+	int ccmax = 0;
 	int ret;
 	if (freed) {
 		if (ppur && *ppur) {
@@ -230,14 +355,8 @@ int list_usb_roots(int freed, pusb_dev_t* ppur, int *psize)
 		return 0;
 	}
 
-	guidstr = (char*)malloc((size_t)ccmax);
-	if (guidstr == NULL) {
-		GETERRNO(ret);
-		goto fail;
-	}
-
-	ret = StringFromGUID2(GUID_DEVINTERFACE_USB_DEVICE,(LPOLESTR)guidstr,ccmax);
-	if (ret == 0) {
+	ret = __get_guid_str((LPGUID)&GUID_DEVINTERFACE_USB_DEVICE,&guidstr,&ccmax);
+	if (ret < 0) {
 		GETERRNO(ret);
 		goto fail;
 	}
@@ -257,10 +376,7 @@ int list_usb_roots(int freed, pusb_dev_t* ppur, int *psize)
 
 	return retlen;
 fail:
-	if (guidstr) {
-		free(guidstr);
-	}
-	guidstr = NULL;
+	__get_guid_str(NULL,&guidstr,&ccmax);
 	SETERRNO(ret);
 	return ret;
 }
