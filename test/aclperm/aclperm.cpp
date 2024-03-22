c622a4b0ff1f9501ccda134c123c925effa34730
jeppeter <jeppeter@gmail.com>
2024-03-23 06:45:41 +0800

    to make aclperm

--- test/aclperm/aclperm.cpp
+++ test/aclperm/aclperm.cpp
@@ -57,291 +57,14 @@ Abstract:
 
 #include <stdio.h>
 #include <assert.h>
+#include <win_priv.h>
+#include <win_output_debug.h>
 
 #define RTN_OK 0
 #define RTN_USAGE 1
 #define RTN_ERROR 13
 
 #pragma comment(lib,"Advapi32.lib")
-
-#define GETERRNO(ret) do{ret = -(int)GetLastError(); if (ret == 0) {ret = -1;}} while(0)
-#define SETERRNO(ret) do{if(ret>0) {SetLastError(ret);} else {SetLastError(-ret);}}while(0)
-
-#define  DEBUG_INFO(...) do{}while(0)
-#define  ERROR_INFO(...) do{}while(0)
-
-
-int AnsiToUnicode(char* pChar, wchar_t **ppWideChar, int*pWideCharSize)
-{
-    wchar_t *pRetWideChar = *ppWideChar;
-    int retwidecharsize = *pWideCharSize;
-    int ret, len, needlen;
-
-    if (pChar == NULL) {
-        if (*ppWideChar) {
-            delete [] pRetWideChar;
-        }
-        *ppWideChar = NULL;
-        *pWideCharSize = 0;
-        return 0;
-    }
-
-    len = (int) strlen(pChar);
-    needlen = MultiByteToWideChar(CP_ACP, 0, pChar, len, NULL, 0);
-    if (retwidecharsize <= needlen) {
-        retwidecharsize = needlen + 1;
-        pRetWideChar = new wchar_t[(size_t)retwidecharsize];
-        assert(pRetWideChar != NULL);
-    }
-
-    ret = MultiByteToWideChar(CP_ACP, 0, pChar, len, pRetWideChar, retwidecharsize);
-    if (ret != needlen) {
-        ret = ERROR_INVALID_BLOCK;
-        goto fail;
-    }
-    pRetWideChar[needlen] = '\0';
-
-    if ( (*ppWideChar) && (*ppWideChar) != pRetWideChar) {
-        wchar_t *pTmpWideChar = *ppWideChar;
-        delete [] pTmpWideChar;
-    }
-    *ppWideChar = pRetWideChar;
-    *pWideCharSize = retwidecharsize;
-    return ret;
-fail:
-    if (pRetWideChar && pRetWideChar != (*ppWideChar)) {
-        delete [] pRetWideChar;
-    }
-    pRetWideChar = NULL;
-    retwidecharsize = 0;
-    SetLastError((DWORD)ret);
-    return -ret;
-}
-
-
-int AnsiToTchar(const char *pChar, TCHAR **pptchar, int *ptcharsize)
-{
-    int ret;
-#ifdef _UNICODE
-    ret = AnsiToUnicode((char*)pChar, pptchar, ptcharsize);
-#else
-    ret = _chartoansi(pChar, pptchar, ptcharsize);
-#endif
-    return ret;
-}
-
-
-int __handle_priv_token(HANDLE htoken, const char* privstr, int enabled)
-{
-    TCHAR* ptpriv = NULL;
-    int tprivsize = 0;
-    int ret;
-    BOOL bret;
-    TOKEN_PRIVILEGES tp;
-    LUID luid;
-
-
-    ret = AnsiToTchar(privstr, &ptpriv, &tprivsize);
-    if (ret < 0) {
-        GETERRNO(ret);
-        goto fail;
-    }
-
-    bret = LookupPrivilegeValue(NULL, ptpriv, &luid);
-    if (!bret) {
-        GETERRNO(ret);
-        ERROR_INFO("lookup [%s] error[%d]", privstr, ret);
-        goto fail;
-    }
-
-    tp.PrivilegeCount = 1;
-    tp.Privileges[0].Luid = luid;
-    if (enabled) {
-        tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
-    } else {
-        tp.Privileges[0].Attributes = 0;
-    }
-
-    bret = AdjustTokenPrivileges(htoken, FALSE, &tp, sizeof(TOKEN_PRIVILEGES), NULL, NULL);
-    if (!bret) {
-        GETERRNO(ret);
-        ERROR_INFO("adjust %s [%s] error[%d]", privstr, enabled ? "enable" : "disable", ret);
-        goto fail;
-    }
-    DEBUG_INFO("%s [%s] succ",  enabled ? "enable" : "disable", privstr);
-
-
-    return 0;
-fail:
-    AnsiToTchar(NULL, &ptpriv, &tprivsize);
-    SETERRNO(ret);
-    return ret;
-}
-
-
-
-int __handle_priv(const char* privstr, int enabled)
-{
-    TCHAR* ptpriv = NULL;
-    int tprivsize = 0;
-    int ret;
-    BOOL bret;
-    HANDLE htoken = NULL;
-
-    bret = OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &htoken);
-    if (!bret) {
-        GETERRNO(ret);
-        ERROR_INFO("open process token error[%d]", ret);
-        goto fail;
-    }
-
-    ret = __handle_priv_token(htoken, privstr, enabled);
-    if (ret < 0) {
-        GETERRNO(ret);
-        goto fail;
-    }
-
-    if (htoken != NULL) {
-        CloseHandle(htoken);
-    }
-    htoken = NULL;
-
-    return 0;
-fail:
-    if (htoken != NULL) {
-        CloseHandle(htoken);
-    }
-    htoken = NULL;
-    AnsiToTchar(NULL, &ptpriv, &tprivsize);
-    SETERRNO(ret);
-    return ret;
-}
-
-int __get_priv_value_inner(HANDLE htoken,const char* privstr)
-{
-    TCHAR* ptpriv = NULL;
-    int tprivsize = 0;
-    int ret;
-    BOOL bret;
-    LUID luid;
-    int value = 0;
-    PVOID ptokenbuf=NULL;
-    DWORD tokenbufsize=32;
-    DWORD dret;
-    TOKEN_PRIVILEGES* ptp=NULL;
-    DWORD i;
-
-
-    ret = AnsiToTchar(privstr, &ptpriv, &tprivsize);
-    if (ret < 0) {
-        GETERRNO(ret);
-        goto fail;
-    }
-
-    bret = LookupPrivilegeValue(NULL, ptpriv, &luid);
-    if (!bret) {
-        GETERRNO(ret);
-        ERROR_INFO("lookup [%s] error[%d]", privstr, ret);
-        goto fail;
-    }
-
-try_again:
-    if (ptokenbuf) {
-        HeapFree(GetProcessHeap(), 0, ptokenbuf);
-    }
-    ptokenbuf = NULL;
-    ptokenbuf = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, tokenbufsize);
-    if (ptokenbuf == NULL) {
-        GETERRNO(ret);
-        goto fail;
-    }
-    bret = GetTokenInformation(htoken,TokenPrivileges,ptokenbuf,tokenbufsize,&dret);
-    if (!bret) {
-        GETERRNO(ret);
-        if (ret == -ERROR_INSUFFICIENT_BUFFER) {
-            tokenbufsize <<= 1;
-            goto try_again;
-        }
-        ERROR_INFO("get token buffer error[%d]", ret);
-        goto fail;
-    }
-
-    ptp = (TOKEN_PRIVILEGES*) ptokenbuf;
-    for (i=0;i<ptp->PrivilegeCount ;i ++) {
-        if (ptp->Privileges[i].Luid.LowPart == luid.LowPart && 
-            ptp->Privileges[i].Luid.HighPart == luid.HighPart) {
-            if (ptp->Privileges[i].Attributes  & SE_PRIVILEGE_ENABLED) {
-                value = 1;
-            } else {
-                value = 0;
-            }
-        }
-    }
-    DEBUG_INFO("[%d] value [%d]", privstr,value);
-
-    if (ptokenbuf) {
-        HeapFree(GetProcessHeap(), 0, ptokenbuf);
-    }
-    ptokenbuf = NULL;
-    AnsiToTchar(NULL, &ptpriv, &tprivsize);
-    return value;
-fail:
-    if (ptokenbuf) {
-        HeapFree(GetProcessHeap(), 0, ptokenbuf);
-    }
-    ptokenbuf = NULL;
-    AnsiToTchar(NULL, &ptpriv, &tprivsize);
-    SETERRNO(ret);
-    return ret;
-}
-
-
-int __get_priv_value(const char* privstr)
-{
-    int ret;
-    int val=0;
-    HANDLE htoken=NULL;
-    BOOL bret;
-
-    bret = OpenProcessToken(GetCurrentProcess(),TOKEN_QUERY,&htoken);
-    if (!bret) {
-        GETERRNO(ret);
-        ERROR_INFO("open token query error[%d]", ret);
-        goto fail;
-    }
-
-    ret = __get_priv_value_inner(htoken,privstr);
-    if (ret < 0) {
-        GETERRNO(ret);
-        goto fail;
-    }
-
-    val = ret;
-
-    if (htoken != NULL && htoken != INVALID_HANDLE_VALUE) {
-        CloseHandle(htoken);
-    }
-    htoken = NULL;
-    return val;
-fail:
-    if (htoken != NULL && htoken != INVALID_HANDLE_VALUE) {
-        CloseHandle(htoken);
-    }
-    htoken = NULL;
-    SETERRNO(ret);
-    return ret;
-}
-
-
-int enable_security_priv(void)
-{
-    return __handle_priv("SeSecurityPrivilege", 1);
-}
-
-int disable_security_priv(void)
-{
-    return __handle_priv("SeSecurityPrivilege", 0);
-}
 
 void
 DisplayLastError(
@@ -355,8 +78,10 @@ main(
     char *argv[]
     )
 {
-    LPTSTR FileName;
-    LPTSTR TrusteeName;
+    TCHAR* FileName=NULL;
+    int filesize=0;
+    TCHAR* TrusteeName=NULL;
+    int trustsize=0;
 
     DWORD AccessMask = GENERIC_ALL;
     DWORD InheritFlag = NO_INHERITANCE;
@@ -376,7 +101,14 @@ main(
         return RTN_USAGE;
     }
 
-    FileName = (LPTSTR)argv[1];
+    ret = AnsiToTchar(argv[1],&FileName,&filesize);
+    if (ret < 0) {
+        GETERRNO(ret);
+        return RTN_USAGE;
+    }
+
+    ret = AnsiToTchar(argv[3],&TrusteeName,&trustsize);
+
     TrusteeName = (LPTSTR)argv[3];
 
     if ( (0 == _stricmp(argv[2], "/Deny") ) ||
