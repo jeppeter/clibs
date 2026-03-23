3cadc82dc2cd285e83623812e753978998d3b114
jeppeter <jeppeter@gmail.com>
2026-03-23 14:53:22 +0800

    ok  in cpp mode for rbtreetest

--- test/rbtreetest/main.cpp
+++ test/rbtreetest/main.cpp
@@ -1,5 +1,13 @@
+#define _HAS_EXCEPTIONS 0
 #include "rbtree.h"
 
+#pragma warning(push)
+
+#if defined(_MSC_VER)
+#if _MSC_VER >= 1910
+#pragma warning(disable:5045)
+#endif
+#endif
 
 
 int main(int argc,char* argv[]) {
@@ -9,6 +17,7 @@ int main(int argc,char* argv[]) {
 	for(i=1;i<argc; i+= 1) {
 		val = atoi(argv[i]);
 		tree.insert(val);
+		fprintf(stdout,"after insert %d\n",val);
 		tree.PrintTree(stdout);
 	}
 
@@ -17,7 +26,11 @@ int main(int argc,char* argv[]) {
 	for(i=1;i<argc;i++) {
 		val = atoi(argv[i]);
 		tree.deleteByVal(val);
+		fprintf(stdout,"after delete %d\n",val);
+		tree.PrintTree(stdout);
 	}
 
 	return 0;
 }
+
+#pragma warning(pop)
