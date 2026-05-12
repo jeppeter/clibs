cb345af8798b607340608ddb8e07ca61f1c1b11d
jeppeter <jeppeter@gmail.com>
2026-05-12 17:30:02 +0800

    coding sqltst

--- test/libtest/sqltst.cpp
+++ test/libtest/sqltst.cpp
@@ -19,6 +19,16 @@ void free_sqlite3_func(psqlite3_func_t* ppfunc)
 	}
 }
 
+#define  GET_FUNC(pfunc,funcname,funcmem,functype)                                                \
+do{                                                                                               \
+	pfunc->funcmem = (functype) ::GetProcAddress(pfunc->m_mod, funcname);                         \
+	if (pfunc->funcmem == NULL) {                                                                 \
+		GETERRNO(ret);                                                                            \
+		ERROR_INFO("GetProcAddress [%s] error %d", funcname, ret);                                \
+		goto fail;                                                                                \
+	}                                                                                             \
+}
+
 psqlite3_func_t init_sqlite3_func(char* dllfile)
 {
 	psqlite3_func_t pfunc = NULL;
@@ -37,16 +47,9 @@ psqlite3_func_t init_sqlite3_func(char* dllfile)
 		goto fail;
 	}
 
-	pfunc->m_openfunc = (sqlite3_open_v2_func_t)GetProcAddress(pfunc->m_mod, "sqlite3_open_v2");
-	if (pfunc->m_openfunc == NULL) {
-		GETERRNO(ret);
-		ERROR_INFO("GetProcAddress [sqlite3_open_v2] error %d", ret);
-		goto fail;
-	}
-
-	pfunc->m_execfunc = (sqlite3_exec_func_t) GetProcAddress(pfunc->m_mod,"sqlite3_exec");
-	
-
+	GET_FUNC(pfunc,"sqlite3_open_v2",m_openfunc,sqlite3_open_v2_func_t);
+	GET_FUNC(pfunc,"sqlite3_exec",m_execfunc,sqlite3_exec_func_t);
+	GET_FUNC(pfunc,"sqlite3_close",m_closefunc,sqlite3_close_func_t);
 
 
 	return pfunc;
@@ -61,14 +64,14 @@ int sql3exec_handler(int argc, char* argv[], pextargs_state_t parsestate, void* 
 {
 	int ret;
 	HMODULE hmod= NULL;
-	sqlite3_open_v2_func_t openfunc = NULL;
-	sqlite3_exec_func_t execfunc = NULL;
-	sqlite3_close_func_t closefunc=NULL;
+	psqlite3_func_t pfunc = NULL;
 	sqlite3* pdb= NULL;
 	char* dbfile = NULL;
 	pargs_options_t pargs = (pargs_options_t) popt;
 
 	init_log_level(pargs);
+
+	for(i=0;)
 
 	if (pargs->m_sqldllfile == NULL) {
 		ret = - ERROR_INVALID_PARAMETER;
@@ -76,19 +79,23 @@ int sql3exec_handler(int argc, char* argv[], pextargs_state_t parsestate, void* 
 		goto out;
 	}
 
-	hmod = LoadLibraryA(pargs->m_sqldllfile);
-	if (hmod == NULL) {
+
+	pfunc = init_sqlite3_func(pargs->m_sqldllfile);
+	if (pfunc == NULL) {
 		GETERRNO(ret);
-		fprintf(stderr,"can not load [%s] error %d\n",pargs->m_sqldllfile,ret);
 		goto out;
 	}
 
-	openfunc = GetProcAddress(hmod,"sqlite3_open_v2");
-	if (openfunc )
-
+	ret = pfunc->m_openfunc()
 
 
 out:
+	if (pdb != NULL) {
+		pfunc->m_closefunc(pdb);
+	}
+	pdb = NULL;
+	free_sqlite3_func(&pfunc);
+
 	SETERRNO(ret);
 	return ret;
 }
